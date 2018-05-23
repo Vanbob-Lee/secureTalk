566b8be504c0537a0ca6173213d7c66b53bbb81c
EvilPuddingLemon <657094295@qq.com>
2018-05-23 11:23:32 +0800

    add position link

--- public/js/chat.js
+++ public/js/chat.js
@@ -67,7 +67,8 @@ function get_pos() {
 var loc_msg;
 function get_address(ret) {
     var rs = ret.result;
-    loc_msg += '，地址：' + rs.formatted_address + '，描述：' + rs.sematic_description;
+    loc_msg += '，地址：' + rs.formatted_address + '，描述：' + rs.sematic_description
+        +'，链接'+'[图片]http://api.map.baidu.com/marker?location='+rs.location.lat+','+rs.location.lng+'&title=我的位置&content='+rs.formatted_address+'&output=html';
     //send_inner(loc_msg);
     send_plain(loc_msg);
 }
